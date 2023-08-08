86cdccd4e07ac2dc7c586b25d7084f11cd47e0c7
ranvitham <107201572+ranvitham@users.noreply.github.com>
2023-08-07 21:46:37 -0500

    Update is_it_a_palindrome.py

--- Python Projects/is_it_a_palindrome.py
+++ Python Projects/is_it_a_palindrome.py
@@ -1,1 +1,19 @@
+"""
+A palindrome is a word, phrase, or sequence that reads the same backward as 
+forward, e.g., madam or nurses run. This function takes in a string and returns
+true or false depending on if it's a palindrome.
+"""
 
+
+def pal_func(x):
+     if " " in x:
+       x = x.lower()
+     if " " in x:
+         x = x.replace(" ", "")
+
+     y = x[::-1]
+
+     if x == y:
+         return True
+     if x !=y:
+         return False
